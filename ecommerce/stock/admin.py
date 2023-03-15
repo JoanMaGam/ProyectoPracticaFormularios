89154ba4097a36682919a61fadf9a2a6a4259e27
Joan Marti <joanmartigamez@gmail.com [core]>
2023-03-15 19:39:51 +0100

    Proyecto modificado. Herramientas de busqueda y filtros añadidos al modelAdmin

--- ecommerce/stock/admin.py
+++ ecommerce/stock/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Product
 
 # Register your models here.
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'short_description', 'stock',)
+    search_fields = ('name', 'short_description',)
+    list_filter = ('name', 'stock', 'discount_until',)
+    date_hierarchy = 'discount_until'
+
+admin.site.register(Product, ProductAdmin)
